dd8b478b494c0a7dd20f20e74ac0e8b5e1597919
Chris <chris.ho@goodstorytime.com>
2024-02-22 12:53:50 +0700

    add webm

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,29 @@
 export default function Home() {
   return (
     <>
-      <main className='flex min-h-screen items-center justify-between p-24 space-x-10'>
+      <main className='grid grid-cols-2 gap-10 p-24'>
         <div>
-          <h3>Load internally (Vercel)</h3>
+          <h3>WebM (Vercel host)</h3>
           <video controls>
-            <source src='/FeverDream.mp4' type='video/mp4' />
+            <source src='/FeverDream.webm' type='video/mp4' />
           </video>
         </div>
-
-        <div>
-          <h3>Load from Cloudfront (AWS)</h3>
-          <video controls>
-            <source
-              src='https://d3a4ovnnqh1r5g.cloudfront.net/videos/FeverDream.mp4'
-              type='video/mp4'
-            />
-          </video>
+        <div className='space-y-5'>
+          <div>
+            <h3>Mp4 (Vercel host)</h3>
+            <video controls>
+              <source src='/FeverDream.mp4' type='video/mp4' />
+            </video>
+          </div>
+          <div>
+            <h3>Mp4 from Cloudfront (AWS)</h3>
+            <video controls>
+              <source
+                src='https://d3a4ovnnqh1r5g.cloudfront.net/videos/FeverDream.mp4'
+                type='video/mp4'
+              />
+            </video>
+          </div>
         </div>
       </main>
     </>
